ccd89c1e37541eba3f64757c0ca066c33f494508
ichsan182 <ichsanilyasa3@gmail.com>
2022-05-29 20:46:24 +0700

    issue : ada kode home yang sama

--- app/Views/layout/page_layout.php
+++ app/Views/layout/page_layout.php
@@ -16,9 +16,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- end font mulish -> googlefonts.com -->
+
+    <!-- css -->
+
+    <link rel="stylesheet" href="css/newcss/home.css">
+    <link rel="stylesheet" href="css/newcss/animate.css">
+    <link rel="icon" href="image/logo.svg" type="image/x-icon">
+
+    <!-- script -->
+    <script id="jquery-core" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" type="text/javascript"></script>
+    <script id="jquery-migrate" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.4.0/jquery-migrate.min.js" type="text/javascript" defer></script>
+    <script id="modernizr" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
 </head>
 
-<body>
+<body data-rsssl=1 id="fyre" class="home page-template-default page page-id-2 sample-page essb-7.9" itemscope itemtype="http://schema.org/Organization">
 
     <?= $this->include('layout/navbar') ?>
     <?= $this->include('layout/header') ?>
@@ -28,6 +39,16 @@
     <!-- Jquery dan Bootsrap JS -->
     <script src="<?= base_url('js/jquery.min.js') ?>"></script>
     <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+
+    <script src="particles.js"></script>
+    <script src="app.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
+
+    <script id="wow" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" type="text/javascript" defer></script>
+
+    <script id="slick" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" type="text/javascript" defer></script>
+
+    <script id="tilt" src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.0.3/tilt.jquery.min.js" type="text/javascript" defer></script>
 
 </body>
 
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -3,6 +3,107 @@
 <?= $this->section('content') ?>
 
 
+    <main class="post-2 page type-page status-publish hentry" style="padding-top:0px;">
+        <section id="home-hero" class="body-color-12431" id="particles-js">
+            <div class="container wide">
+                <div class="row">
+                    <div class="col-12 col-sm-12 offset-md-1 col-md-10 col-lg-6 offset-lg-0 offset-xl-0 col-xl-6">
+                        <div>
+                            <h2 class="wow fadeInUp">HOME</h2>
+                            <h1 class="wow fadeInUp">
+                                METROKIL
+                            </h1>
+                            <p class="description wow fadeInUp" itemprop="description">
+                                Setelah diakui sebagai salah perusahaan pengusir hama yang ampuh, kini metrokill meyediakan produk pengusir hama yang berkualitas.
+                            </p>
+
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-12 offset-md-1 col-md-10 col-lg-6 offset-lg-0 col-xl-6">
+
+                        <div class="featured-case-study">
+                            <div class="case-study-content-contain">
+                                <h3 class="h6 wow fadeInUp">DEDICATED TO <br>ENVIRONMENT</h3>
+                            </div>
+                            <div class="case-study-img-contain">
+                                <div class="case-study-img wow fadeInUp" data-wow-delay=".5s">
+                                    <img src="image/Spray can.svg" height="400" width="239" alt="State 48 Case Study - ecommerce development by Fyresite" />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+
+        <script type="text/javascript" defer async>
+            jQuery(document).ready(function($) {
+                // VIDEO CONTROLS
+                var myVideo = document.getElementById("home-vid");
+                // VIDEO PLAY FUNCTION
+                function playPause() {
+                    if (myVideo.paused) {
+                        myVideo.play();
+                        $('.play-button-replace').addClass('fa-pause');
+                        $('.play-button-replace').removeClass('fa-play');
+                    } else {
+                        myVideo.pause();
+                        $('.play-button-replace').removeClass('fa-pause');
+                        $('.play-button-replace').addClass('fa-play');
+                    }
+                }
+                $('#play-home-vid').on('click', playPause);
+
+                // WOW ANIMATIONS
+                wow = new WOW({
+                    boxClass: 'wow',
+                    animateClass: 'animated',
+                    offset: 0,
+                    mobile: true,
+                    live: true
+                })
+                wow.init();
+
+                // TILT
+                if (Modernizr.touch) {
+                    $('.tilt, .post-tilt').on('touchstart touchend', function(e) {
+                        // e.preventDefault();
+                        $(this).toggleClass('hover');
+                    });
+                } else {
+                    var timesRun = 0;
+                    var interval = setInterval(function() {
+                        timesRun += 1;
+                        if (timesRun === 30) {
+                            clearInterval(interval);
+                        }
+                        $('.tilt').tilt({
+                            // scale: 1.0125,
+                            glare: true,
+                            maxGlare: .25
+                        });
+                        $('.post-tilt').tilt({
+                            // scale: 1.0125,
+                            glare: false,
+                            maxGlare: .25
+                        });
+                        $('.home-post-tilt').tilt({
+                            // scale: 1.0125,
+                            maxTilt: 20,
+                            perspective: 1000,
+                            glare: false,
+                            maxGlare: .25
+                        });
+                    }, 1000);
+                }
+            });
+        </script>
+    </main>
+
 
 
 <?= $this->endSection() ?>

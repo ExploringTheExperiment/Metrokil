a6dae0609df3944b60521920a7145b05938161cf
michael <alf.michael29@gmail.com>
2022-06-11 20:37:43 +0700

    fix

--- app/Views/layout/page_layout.php
+++ app/Views/layout/page_layout.php
@@ -16,6 +16,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <!-- end font mulish -> googlefonts.com -->
+
+    <!-- Link to the file hosted on your server, -->
+    <link rel="stylesheet" href="/node_modules/@splidejs/splide/dist/css/splide.min.css">
+
 </head>
 
 <body>
@@ -29,6 +33,7 @@
     <script src="<?= base_url('js/jquery.min.js') ?>"></script>
     <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
 
+    <script src="/node_modules/@splidejs/splide/dist/js/splide.min.js"></script>
 </body>
 
 </html>
--- app/Views/pages/product.php
+++ app/Views/pages/product.php
@@ -16,21 +16,36 @@
     }
 </style>
 
- <div class="container px-md-5">
+
+<div class="container px-md-5">
     <div class="row pt-md-5 pt-5">
         <div class="col pt-3">
             <img src="../image/Product.svg">
             <h1 class="fw-bolder">METROKIL</h1>
             <h1 class="fw-bolder">PRODUCT</h1>
             <p class="fw-lighter fs-6 py-md-2 py-2">Setelah jasa yang kami tawarkan, kami<br>juga menwarkan produk andalan kami.</p>
+        </div>
+        <div class="col d-flex justify-content-center">
+            <img src="../image/shopping_Bag.svg">
+        </div>
     </div>
-    <div class="col d-flex justify-content-center">
-        <img src="../image/shopping_Bag.svg">
+    <div class="row pt-md-5 pt-5">
+        <section class="splide" aria-labelledby="carousel-heading">
+            <h2 id="carousel-heading">Splide Basic HTML Example</h2>
+
+
+            <div class="splide__track">
+                <ul class="splide__list">
+                    <li class="splide__slide">Slide 01</li>
+                    <li class="splide__slide">Slide 02</li>
+                    <li class="splide__slide">Slide 03</li>
+                </ul>
+            </div>
+        </section>
     </div>
-  </div>
 </div>
 
- 
+
 
 
 <?= $this->endSection() ?>

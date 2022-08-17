02093113a5da8045dc3872fb9e0f52bf61e26d15
ichsan182 <ichsanilyasa3@gmail.com>
2022-08-17 15:29:38 +0700

    Update about.php

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -3,6 +3,277 @@
 <?= $this->section('content') ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <style>
+        body {
+            margin: 0;
+            font-family: var(--bs-body-font-family);
+            font-size: var(--bs-body-font-size);
+            font-weight: var(--bs-body-font-weight);
+            line-height: var(--bs-body-line-height);
+            color: var(--bs-body-color);
+            text-align: var(--bs-body-text-align);
+            background-color: var(--bs-body-bg);
+            -webkit-text-size-adjust: 100%;
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        }
+
+        .d-none-126412 {
+            display: none !important;
+        }
+
+        .container,
+        .container-fluid-12479,
+        .container-xxl,
+        .container-xl,
+        .container-lg,
+        .container-md,
+        .container-sm {
+            width: 100%;
+            padding-right: var(--bs-gutter-x, 0.75rem);
+            padding-left: var(--bs-gutter-x, 0.75rem);
+            margin-right: auto;
+            margin-left: auto;
+        }
+
+        .navbar>.container,
+        .navbar>.container-fluid-12479,
+        .navbar>.container-sm,
+        .navbar>.container-md,
+        .navbar>.container-lg,
+        .navbar>.container-xl,
+        .navbar>.container-xxl {
+            display: flex;
+            flex-wrap: inherit;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        .p-0-1274 {
+            padding: 0 !important;
+        }
+
+        @media (min-width: 992px) {
+            .d-lg-1294717 {
+                display: block !important;
+            }
+        }
+
+        section.resume-content-192924 {
+            display: flex;
+            align-items: center;
+            padding-left: 1rem;
+            padding-right: 1rem;
+            padding-top: 5rem;
+            padding-bottom: 5rem;
+            max-width: 75rem;
+        }
+
+        section.resume-content-192924 .resume-content-192924-content {
+            width: 100%;
+        }
+
+        @media (min-width: 768px) {
+            section.resume-content-192924 {
+                min-height: 80vh;
+            }
+        }
+
+        @media (min-width: 992px) {
+            section.resume-content-192924 {
+                background-color: #fff;
+                padding-left: 30px;
+                padding-right: 3rem;
+                padding-top: 5rem;
+
+            }
+        }
+
+        .mb-0-nw-mb {
+            margin-bottom: 0 !important;
+        }
+
+        .mb-5-anw-2y4 {
+            margin-bottom: 3rem !important;
+        }
+
+        p.lead-2288cs {
+            font-size: 1.15rem;
+            font-weight: 400;
+        }
+
+        .lead-2288cs {
+            font-size: 1.25rem;
+            font-weight: 300;
+        }
+
+        .list-group-numbered {
+            list-style-type: none;
+            counter-reset: section;
+        }
+
+        .list-group-numbered>li::before {
+            content: counters(section, ".") ". ";
+            counter-increment: section;
+        }
+
+        h1,
+        .h1,
+        h2,
+        .h2,
+        h3,
+        .h3,
+        h4,
+        .h4,
+        h5,
+        .h5,
+        h6,
+        .h6 {
+            text-transform: uppercase;
+        }
+
+        h1,
+        .h1 {
+            line-height: 1;
+        }
+
+        h1,
+        .h1 {
+            font-size: calc(1.725rem + 5.7vw);
+        }
+
+        @media (min-width: 1200px) {
+
+            h1,
+            .h1 {
+                font-size: 6rem;
+            }
+        }
+
+        h4,
+        .h4 {
+            font-size: calc(1.275rem + 0.3vw);
+        }
+
+        @media (min-width: 1200px) {
+
+            h4,
+            .h4 {
+                font-size: 1.5rem;
+            }
+        }
+
+        p {
+            margin-top: 0;
+            margin-bottom: 1rem;
+        }
+
+        h6,
+        .h6,
+        h5,
+        .h5,
+        h4,
+        .h4,
+        h3,
+        .h3,
+        h2,
+        .h2,
+        h1,
+        .h1 {
+            margin-top: 0;
+            margin-bottom: 0.5rem;
+            font-family: "Saira Extra Condensed", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            font-weight: 700;
+            line-height: 1.2;
+            color: #343a40;
+        }
+
+        p {
+            display: block;
+            margin-block-start: 1em;
+            margin-block-end: 1em;
+            margin-inline-start: 0px;
+            margin-inline-end: 0px;
+        }
+
+        p {
+            margin-top: 0;
+            margin-bottom: 1rem;
+        }
+
+        .navbar-12822 {
+            padding-left: 0;
+            padding-right: 0;
+            text-align: center;
+            position: relative;
+            top: 0;
+            left: 0;
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+        }
+
+        .page {
+            display: flex;
+        }
+    </style>
+</head>
+
+
+<body>
+    <div class="page">
+        <link rel="stylesheet" href="css/button.css">
+        <!-- Navigation-->
+        <span class="d-none-126412 d-lg-1294717">
+            <img src="/image/gambar.jpg" class="navbar-12822">
+        </span>
+        <!-- Page Content-->
+        <div class="container-fluid-12479 p-0-1274">
+            <!-- About-->
+            <section class="resume-content-192924">
+                <div class="resume-content-192924-content">
+                    <h4 class="mb-0-nw-mb"> About Us</h4>
+                    <h1 class="mb-0-nw-mb">
+                        ABOUT<br>METROKIL
+                    </h1>
+                    <p class="lead-2288cs mb-5-anw-2y4">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Debitis veniam odit repellendus cumque! Eius vel amet ratione aut maiores neque ipsam, tenetur
+                        molestiae inventore et repellat porro nostrum dolorem minima! Lorem ipsum dolor,
+                        sit amet consectetur adipisicing elit. Neque ad iste distinctio ipsa culpa. Nisi ratione illo
+                        recusandae cumque voluptatum est qui nesciunt quibusdam beatae ducimus nam, nemo, sunt illum.
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Vitae amet libero voluptatibus officiis totam accusamus quas, necessitatibus
+                        itaque dolorem rerum facere assumenda? Asperiores voluptas maiores aperiam! Provident est
+                        perspiciatis eaque!</p>
+
+                    <div class="social-icons szier-12">
+                        <a class="animated-arrow-12864" href="aboutdetail.html">
+                            <span class="the-arrow-16442 -left-12385">
+                                <span class="shaft-127329"></span>
+                            </span>
+                            <span class="main-1251922254">
+                                <span class="text-19223">
+                                    Want to explore more?
+                                </span>
+                                <span class="the-arrow-16442 -right">
+                                    <span class="shaft-127329"></span>
+                                </span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</body>
+
+
+<script src="particles.js"></script>
+<script src="app.js"></script>
+
+</html>
 
 <?= $this->endSection() ?>
